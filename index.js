2173d047731fe047f1655dc28110c0e0f481f09b
Paul Unsted <ed__84@hotmail.com>
2019-08-17 17:42:18 -0400

    renamed server.js to conform with my npm init

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+// html routing, have to move these into their own js files in the routing subdir later
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/app/public/home.html"));
   });
@@ -20,3 +20,9 @@ app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "/app/public/survey.html"));
   });
 
+
+
+// Starts the server and begins listening  
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
