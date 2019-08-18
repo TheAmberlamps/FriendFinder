3c74dbd5bb4fc33ab4cc795f732c01452093d035
Paul Unsted <ed__84@hotmail.com>
2019-08-18 01:43:57 -0400

    lifted and installed the example head and body sections into my html, tweaked with the js a bit

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "/app/public/survey.html"));
   });
 
-
-
 // Starts the server and begins listening  
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
